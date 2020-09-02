50fdb7deadfb85a58298f88f99c929006ca1e666
Travis CI User <travis@example.org>
2020-09-02 13:08:06 +0000

    Updates

---
+++ component---src-pages-index-js-64cac3282075b5457a35.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(n,t,e){"use strict";e.r(t),e.d(t,"default",(function(){return c}));var r=e("q1tI"),u=e.n(r);function c(){return u.a.createElement("div",null,"Travis Works!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-64cac3282075b5457a35.js.map
