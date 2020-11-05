3a0b3375204f794b1e2033fcd8c1674c47962e00
Oindrilla Chatterjee <ochatter@redhat.com>
2020-11-05 10:21:37 -0500

    Modified command to output local branch (#65)

--- scripts/gh-pages-branch-fork.sh
+++ scripts/gh-pages-branch-fork.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 printf "\n\n######## build & deploy GitHub Pages from a fork ########\n"
-printf "\n## Info: Non-destructive build. Result is deployed on \"gh-pages\" branch in \"$(git branch --show-current)\" folder.\n\n"
+printf "\n## Info: Non-destructive build. Result is deployed on \"gh-pages\" branch in \"$(git rev-parse --abbrev-ref HEAD)\" folder.\n\n"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
