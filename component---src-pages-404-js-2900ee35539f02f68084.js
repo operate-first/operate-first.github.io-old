bc2b19729e231ccdef61d597db18f4f1f867dcd3
Travis CI User <travis@example.org>
2020-12-08 14:13:40 +0000

    Updates 3dd6314c703260fc08c69503f926db3059a82ec3: upgraded dependencies (#151)

---
+++ component---src-pages-404-js-2900ee35539f02f68084.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),o=a.n(n),l=a("Zttt"),i=a("vrFN");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return o.a.createElement(l.c,{location:a,title:n},o.a.createElement(i.a,{title:"404: Not Found"}),o.a.createElement("h1",null,"Not Found"),o.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2900ee35539f02f68084.js.map
