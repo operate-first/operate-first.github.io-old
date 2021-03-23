92c0da5168c2f8526cf95a9fd3f2bac3bdce6b08
Tomas Coufal <tcoufal@redhat.com>
2021-03-23 13:42:14 +0100

    feat: Add HeaderTools with YouTube/Github/Slack links
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Link } from "gatsby";
-import { Brand, PageHeader, Nav, NavList, NavItem } from "@patternfly/react-core";
+import { Link, useStaticQuery, graphql } from "gatsby";
+import { Brand, PageHeader, PageHeaderTools, PageHeaderToolsItem, Nav, NavList, NavItem, Button } from "@patternfly/react-core";
 import logo from "./logo.png";
+import GithubIcon from '@patternfly/react-icons/dist/esm/icons/github-icon';
+import SlackIcon from '@patternfly/react-icons/dist/esm/icons/slack-icon';
+import YoutubeIcon from '@patternfly/react-icons/dist/esm/icons/youtube-icon';
 
 function createNavItem(id, label, href, pathname) {
   let isActive = pathname.startsWith(href);
@@ -26,6 +29,58 @@ function topNav(location) {
   )
 };
 
+const HeaderTools = () => {
+  const { site: { siteMetadata: links } } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            github
+            youtube
+            slack
+          }
+        }
+      }
+    `
+  );
+
+  const headerTools = [
+    {
+      href: links.github,
+      ariaLabel: "Operate First GitHub organization",
+      icon: <GithubIcon />,
+    },
+    {
+      href: links.slack,
+      ariaLabel: "Operate First Slack workspace",
+      icon: <SlackIcon />,
+    },
+    {
+      href: links.youtube,
+      ariaLabel: "Operate First YouTube",
+      icon: <YoutubeIcon />,
+    }
+  ];
+
+  return (
+    <PageHeaderTools>
+      { headerTools.map(t => (
+        <PageHeaderToolsItem key={ t.href }>
+          <Button
+          component='a'
+          variant='plain'
+          href={ t.href }
+          target="top"
+          aria-label={ t.ariaLabel }
+          >
+            { t.icon }
+          </Button>
+        </PageHeaderToolsItem>
+      ))}
+    </PageHeaderTools>
+  );
+}
+
 export const Header = ({ isNavOpen, onNavToggle, location }) => (
   <PageHeader
     className="header"
@@ -35,5 +90,6 @@ export const Header = ({ isNavOpen, onNavToggle, location }) => (
     isNavOpen={isNavOpen}
     onNavToggle={onNavToggle}
     topNav={topNav(location)}
+    headerTools={ <HeaderTools /> }
   />
 );
